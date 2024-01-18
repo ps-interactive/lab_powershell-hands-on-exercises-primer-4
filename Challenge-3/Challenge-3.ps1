8050bc40aca23ed5c79384f4b35689cc90114d40
Liam Cleary <liamcleary@msn.com>
2024-01-18 17:28:24 -0500

    Minor adjustments based on testing

--- Challenge-3/Challenge-3.ps1
+++ Challenge-3/Challenge-3.ps1
@@ -1,12 +1,8 @@
 ############################################
 ## Step 1: Set Variables and Prerequistes ##
 ############################################
-Install-Module -Name SqlServer
 Import-Module SqlServer
-
-$sqlInstance = "localhost"
-$username = "sa"
-$password = "Pass@word1"
+$sqlInstance = "."
 
 ######################################
 ## Step 2: Backing Up SQL Databases ##
@@ -17,23 +13,17 @@ $databaseName = "AdventureWorks2022"
 $path = "C:\PowerShell\Database"
 
 # Backup the Complete Database
-$backupPath = "$path\AdventureWorks2022.bak"
+$backupPath = "$path\AdventureWorks2022-Regular.bak"
 Backup-SqlDatabase -ServerInstance $sqlInstance -Database $databaseName
 
 # Backup the Complete Database to the Specific Path
-$backupPath = "$path\AdventureWorks2022.bak"
+$backupPath = "$path\AdventureWorks2022-With-Path.bak"
 Backup-SqlDatabase -ServerInstance $sqlInstance -Database $databaseName -BackupFile $backupPath
-
-# Backup a Database based on Location
-Set-Location "SQLSERVER:\SQL\$sqlInstance\DEFAULT\Databases\$databaseName"
-Backup-SqlDatabase -Database $databaseName
 
 # Set Database Logging to Full
 $Query = "ALTER DATABASE [$databaseName] SET RECOVERY FULL;"
-Invoke-Sqlcmd -ServerInstance $sqlInstance -Query $Query -Username $username -Password $password -TrustServerCertificate:$true
-
-# Do a Full Backup
-$backupPath = "$path\AdventureWorks2022.bak"
+Invoke-Sqlcmd -ServerInstance $sqlInstance -Query $Query -TrustServerCertificate:$true
+$backupPath = "$path\AdventureWorks2022-Full.bak"
 Backup-SqlDatabase -ServerInstance $sqlInstance -Database $databaseName -BackupFile $backupPath -BackupAction Database
 
 # Backup the Transaction Log
@@ -42,20 +32,11 @@ Backup-SqlDatabase -ServerInstance $sqlInstance -Database $databaseName -BackupF
 
 # Set Database Logging to Simple
 $Query = "ALTER DATABASE [$databaseName] SET RECOVERY SIMPLE;"
-Invoke-Sqlcmd -ServerInstance $sqlInstance -Query $Query -Username $username -Password $password -TrustServerCertificate:$true
+Invoke-Sqlcmd -ServerInstance $sqlInstance -Query $Query -TrustServerCertificate:$true
 
 # Create a Differential Backup
 $backupPath = "$path\AdventureWorks2022.dif"
 Backup-SqlDatabase -ServerInstance $sqlInstance -Database $databaseName -BackupFile $backupPath -BackupAction Database -Incremental
-
-# Create a Compressed Backup
-$backupPath = "$path\AdventureWorks2022.cmp"
-Backup-SqlDatabase -ServerInstance $sqlInstance -Database $databaseName -BackupFile $backupPath -CompressionOption On
-
-# Create an Encrypted Backup
-$backupPath = "$path\AdventureWorks2022.enc"
-$EncryptionOption = New-SqlBackupEncryptionOption -Algorithm Aes256 -EncryptorType ServerCertificate -EncryptorName "BackupCert"
-Backup-SqlDatabase -ServerInstance $sqlInstance -Database $databaseName -BackupFile $backupPath -EncryptionOption $EncryptionOption
 
 
 
@@ -69,10 +50,6 @@ $path = "C:\PowerShell\Database"
 $backupFile = "$path\AdventureWorks2022.bak"
 Restore-SqlDatabase -ServerInstance $sqlInstance -Database $databaseName -BackupFile $backupFile
 
-# Restore a Database Transaction Log
-$backupFile = "$path\AdventureWorks2022.trn"
-Restore-SqlDatabase -ServerInstance $sqlInstance -Database $databaseName -BackupFile $backupFile -RestoreAction Log
-
 # Restore a Database with Replace
 $backupFile = "$path\AdventureWorks2022.bak"
 Restore-SqlDatabase -ServerInstance $sqlInstance -Database $databaseName -BackupFile $backupFile -ReplaceDatabase
@@ -83,7 +60,7 @@ Restore-SqlDatabase -ServerInstance $sqlInstance -Database $databaseName -Backup
 
 # Restore and Create a New Database
 $backupFile = "$path\AdventureWorks2022.bak"
-$sqlFilePath = "C:\Program Files\Microsoft SQL Server\MSSQL16.MSSQLSERVER\MSSQL\DATA"
+$sqlFilePath = "C:\Program Files\Microsoft SQL Server\MSSQL16.SQLEXPRESS\MSSQL\DATA"
 $newDatabaseName = "AdventureWorks2023"
 $mdfPath = "$sqlFilePath\AdventureWorks2023.mdf"
 $ldfPath = "$sqlFilePath\AdventureWorks2023_log.ldf"
@@ -111,7 +88,7 @@ function Backup-Database {
 
     Write-Host "Setting database recovery mode to FULL..."
     $Query = "ALTER DATABASE [$databaseName] SET RECOVERY FULL;"
-    Invoke-Sqlcmd -ServerInstance $sqlInstance -Query $Query -Username $username -Password $password -TrustServerCertificate:$true
+    Invoke-Sqlcmd -ServerInstance $sqlInstance -Query $Query -TrustServerCertificate:$true
     if ($?) {
         Write-Host "Database recovery mode set to FULL successfully."
     } else {
@@ -130,7 +107,7 @@ function Backup-Database {
 
     Write-Host "Setting database recovery mode to SIMPLE..."
     $Query = "ALTER DATABASE [$databaseName] SET RECOVERY SIMPLE;"
-    Invoke-Sqlcmd -ServerInstance $sqlInstance -Query $Query -Username $username -Password $password -TrustServerCertificate:$true
+    Invoke-Sqlcmd -ServerInstance $sqlInstance -Query $Query -TrustServerCertificate:$true
     if ($?) {
         Write-Host "Database recovery mode set to SIMPLE successfully."
     } else {
@@ -142,9 +119,9 @@ function Backup-Database {
 }
 
 # Execute the Function
-$databaseName = "AdventureWorks2022"
+$databaseName = "AdventureWorks2023"
 $path = "C:\PowerShell\Database"
-$backupPath = "$path\AdventureWorks2022-F.bak"
+$backupPath = "$path\AdventureWorks2023-F.bak"
 Backup-Database -sqlInstance $sqlInstance -databaseName $databaseName -backupPath $backupPath
 
 
@@ -173,53 +150,14 @@ function Restore-Database {
 }
 
 # Execute the Function
-$databaseName = "AdventureWorks2022"
+$databaseName = "AdventureWorks2023"
 $path = "C:\PowerShell\Database"
-$backupPath = "$path\AdventureWorks2022-F.bak"
+$backupPath = "$path\AdventureWorks2023-F.bak"
 Restore-Database -sqlInstance $sqlInstance -databaseName $databaseName -backupPath $backupPath
 
 
-# Function for Restoring a Database as a New Database and Providing Logging
-function Restore-DatabaseAsNew {
-    param (
-        [Parameter(Mandatory=$true)]
-        [string]$sqlInstance,
-        [Parameter(Mandatory=$true)]
-        [string]$databaseName,
-        [Parameter(Mandatory=$true)]
-        [string]$backupPath,
-        [Parameter(Mandatory=$true)]
-        [string]$newDatabaseName
-    )
-    $success = $true
-
-    Write-Host "Restoring the database..."
-    $sqlFilePath = "C:\Program Files\Microsoft SQL Server\MSSQL16.MSSQLSERVER\MSSQL\DATA"
-    $mdfPath = "$sqlFilePath\$newDatabaseName.mdf"
-    $ldfPath = "$sqlFilePath\$newDatabaseName_log.ldf"
-    $data = New-Object Microsoft.SqlServer.Management.Smo.RelocateFile("$($databaseName)", "$mdfPath")
-    $log = New-Object Microsoft.SqlServer.Management.Smo.RelocateFile("$($databaseName)_Log", "$ldfPath")
-    Restore-SqlDatabase -ServerInstance $sqlInstance -Database $newDatabaseName -BackupFile "$backupPath" -RelocateFile @($data,$log)
-    if ($?) {
-        Write-Host "Database restore completed successfully."
-    } else {
-        Write-Host "Failed to restore the database."
-        $success = $false
-    }
-
-    return $success
-}
-
-# Execute the Function
-$databaseName = "AdventureWorks2022"
-$path = "C:\PowerShell\Database"
-$backupPath = "$path\AdventureWorks2022-F.bak"
-$newDatabaseName = "AdventureWorks2023"
-Restore-DatabaseAsNew -sqlInstance $sqlInstance -databaseName $databaseName -backupPath $backupPath -newDatabaseName $newDatabaseName
-
-
-# Function to Backup ALL Databases and Provide a CSV Report of the Backup
-function Backup-AllDatabases {
+# Function to Backup Database and Provide a CSV Report of the Backup
+function Backup-Database {
     param (
         [Parameter(Mandatory=$true)]
         [string]$sqlInstance,
@@ -231,7 +169,7 @@ function Backup-AllDatabases {
 
     Write-Host "Setting database recovery mode to FULL..."
     $Query = "ALTER DATABASE [$databaseName] SET RECOVERY FULL;"
-    Invoke-Sqlcmd -ServerInstance $sqlInstance -Query $Query -Username $username -Password $password -TrustServerCertificate:$true
+    Invoke-Sqlcmd -ServerInstance $sqlInstance -Query $Query -TrustServerCertificate:$true
     if ($?) {
         Write-Host "Database recovery mode set to FULL successfully."
         $output += [PSCustomObject]@{
@@ -266,7 +204,7 @@ function Backup-AllDatabases {
 
     Write-Host "Setting database recovery mode to SIMPLE..."
     $Query = "ALTER DATABASE [$databaseName] SET RECOVERY SIMPLE;"
-    Invoke-Sqlcmd -ServerInstance $sqlInstance -Query $Query -Username $username -Password $password -TrustServerCertificate:$true
+    Invoke-Sqlcmd -ServerInstance $sqlInstance -Query $Query -TrustServerCertificate:$true
     if ($?) {
         Write-Host "Database recovery mode set to SIMPLE successfully."
         $output += [PSCustomObject]@{
@@ -289,11 +227,5 @@ function Backup-AllDatabases {
 
 # Execute the Function
 $path = "C:\PowerShell\Database"
-$backupPath = "$path\AdventureWorks2022-F.bak"
-Backup-AllDatabases -sqlInstance $sqlInstance -backupPath $backupPath
-
-
-
-
-
-
+$backupPath = "$path\AdventureWorks2023-Report.bak"
+Backup-Database -sqlInstance $sqlInstance -backupPath $backupPath
